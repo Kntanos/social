37f3b748db2878f4fcc4a208083ab844dfbc217f
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-08-10 16:36:37 +0100

    put blogs in descending order

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,8 @@ class BlogsController < ApplicationController
   
   # GET /blogs or /blogs.json
   def index
-    @blogs = current_user.blogs #Blog.all
+    #Blog.all
+    @blogs = current_user.blogs.order(:created_at).reverse 
   end
 
   # GET /blogs/1 or /blogs/1.json
