eea47d78d7387a49e62ffa2aae82b30b97959b59
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-08-11 12:44:55 +0100

    before attemping to do more with devise and user model

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs or /blogs.json
   def index
     #Blog.all
-    @blogs = current_user.blogs.order(:created_at).reverse 
+    @blogs = current_user.blogs.order(:created_at).reverse  # however this works too: blogs.order("created_at DESC") 
   end
 
   # GET /blogs/1 or /blogs/1.json
